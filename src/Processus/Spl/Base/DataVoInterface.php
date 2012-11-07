3f51e4fbbb1801f38ea8d7bf96a10b172a2b2c43
FrancisVarga <github@varga-net.com>
2012-11-07 13:27:30 +0100

    initial commit; ignore idea

---
+++ src/Processus/Spl/Base/DataVoInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hissterkiller
+ * Date: 11/7/12
+ * Time: 12:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Spl\Interfaces;
+interface DataVoInterface
+{
+
+}
