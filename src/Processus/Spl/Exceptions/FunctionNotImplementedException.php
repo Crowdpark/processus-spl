3cb8b0c1e66b4dfcdee760d1570ec0b6e00f5dd3
FrancisVarga <github@varga-net.com>
2012-11-13 14:09:36 +0100

    adding new Exception

---
+++ src/Processus/Spl/Exceptions/FunctionNotImplementedException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hissterkiller
+ * Date: 11/13/12
+ * Time: 2:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class FunctionNotImplementedException extends Exception
+{
+    public function __construct($message = "", $code = 0, Exception $previous = null)
+    {
+        $message = "Function not implemented in this Class.";
+        parent::__construct($message, $code, $previous);
+    }
+
+}
